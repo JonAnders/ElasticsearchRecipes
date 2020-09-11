b32ecc634c6bb296ba95ee53163b9610b566c2b4
Jon Anders Amundsen <jon.a.amundsen@gmail.com>
2020-09-11 17:13:40 +0200

    Add basic skeleton of web app

---
+++ ElasticsearchRecipes.Web/Controllers/RecipesController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ElasticsearchRecipes.Web.Models;
+
+namespace ElasticsearchRecipes.Web.Controllers
+{
+    public class RecipesController : Controller
+    {
+        private readonly ILogger<RecipesController> _logger;
+
+        public RecipesController(ILogger<RecipesController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ElasticsearchRecipes.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ElasticsearchRecipes.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
